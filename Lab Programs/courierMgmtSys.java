ff0ea883d7068d7141274d62bbc362b91af52e05
Akash Roshan <akash.second1999@gmail.com>
2021-02-11 15:09:59 +0530

    lots of stuff, dont wanna talk about it

--- Lab Programs/courierMgmtSys.java
+++ Lab Programs/courierMgmtSys.java
@@ -14,9 +14,12 @@ class courierMgmtSys {
 	static int managerID = 1;
 	static int adminID = 1;
 	static int courierID = 1;
+	static int shipmentID = 1;
+
 	static Admin adminObjects[] = new Admin[10];
 	static Manager managerObjects[] = new Manager[10];
 	static Courier courierObjects[] = new Courier[10];
+	static Shipment shipmentObjects[] = new Shipment[10];
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -24,7 +27,7 @@ class courierMgmtSys {
 
 		do {
 			System.out.println("\tCourier Management System");
-			System.out.println("1. Admin Module\n2. Manager Module\n3. Courier Module\n4. Exit");
+			System.out.println("1. Admin Module\n2. Manager Module\n3. Courier Module\n4. Shipment Module\n5. Exit");
 			System.out.println("Enter your choice:");
 			choice = Integer.parseInt(br.readLine());
 
@@ -38,11 +41,14 @@ class courierMgmtSys {
 				case 3:
 					courier_program();
 					break;
+				case 4:
+					shipment_program();
+					break;
 				default:
 					System.out.println("\nProgram will exit");
 					break;
 			}
-		} while (choice < 4);
+		} while (choice < 5);
 	}
 
 	public static int menu(int i) throws IOException {
@@ -126,7 +132,7 @@ class courierMgmtSys {
 		} while (choice < 4);
 	}
 
-	// allows user to use admin module
+	// allows user to use courier module
 	public static void courier_program() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int choice;
@@ -161,6 +167,59 @@ class courierMgmtSys {
 			System.out.println("\nPress Enter to Continue...");
 			br.readLine();
 		} while (choice < 4);
+	}
+
+	// allows user to use courier module
+	public static void shipment_program() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int choice;
+		int id;
+
+		do {
+			System.out.println("\tCheck Shipments");
+			System.out.println("1. Add new Shipment\n2. Update Status\n3. Update Location details\n4. Display Shipment\n5. Go Back");
+			System.out.println("Enter your choice:");
+			choice =  Integer.parseInt(br.readLine());
+
+			switch (choice) {
+				case 1:
+					shipmentObjects[shipmentID++] = new Shipment();
+					break;
+				case 2:
+					System.out.println("Enter Shipment ID:");
+					id = Integer.parseInt(br.readLine());
+					shipmentObjects[id].update_status();
+					System.out.println("Modified Details:\n");
+					shipmentObjects[id].display();
+					break;
+				case 3:
+					System.out.println("Enter Shipment ID:");
+					id = Integer.parseInt(br.readLine());
+					shipmentObjects[id].update_details();
+					System.out.println("Modified Details:\n");
+					shipmentObjects[id].display();
+					break;
+				case 4:
+					System.out.println("Enter Shipment ID:");
+					id = Integer.parseInt(br.readLine());
+					shipmentObjects[id].display();
+					break;
+				case 5:
+					System.out.println("*********All Shipment Details********\n");
+					for(int i = 1; i < shipmentID; i++) {
+						shipmentObjects[i].display();
+						System.out.println("*************************************");
+						System.out.println("*************************************");
+					}
+					break;
+				default:
+					System.out.println("\nProgram will go to the main menu\n");
+					return;
+			}
+
+			System.out.println("\nPress Enter to Continue...");
+			br.readLine();
+		} while (choice < 6);
 	}
 
 }
@@ -228,11 +287,11 @@ class Person {
 }
 
 
-// class admin for admins
+// class admin used for admins
 class Admin extends Person {
+	static int id = 1;
 	int Admin_ID;
 	private String Admin_Password;
-	static int id = 1;
 
 	// default constructor is used to assign default data to a new object
 	Admin() throws IOException {
@@ -266,11 +325,11 @@ class Admin extends Person {
 
 // class manager used for managers
 class Manager extends Person {
+	static int id = 1;
 	int Manager_ID;
 	private String Manager_Password;
 	String Manager_Hub;
 	float Manager_Salary;
-	static int id = 1;
 
 	// default constructor is used to assign default data to a new object. Will not be used so can be removed
 	Manager() throws IOException {
@@ -319,14 +378,14 @@ class Manager extends Person {
 	}
 }
 
-// class manager used for managers
+// class courier used for couriers
 class Courier extends Person {
+	static int id = 1;
 	int Courier_ID;
 	private String Courier_Password;
 	String Courier_Hub;
 	String Courier_Manager;
 	float Courier_Salary;
-	static int id = 1;
 
 	// default constructor is used to assign default data to a new object. Will not be used so can be removed
 	Courier() throws IOException {
@@ -379,6 +438,140 @@ class Courier extends Person {
 		System.out.println("Hub      : " + Courier_Hub);
 		System.out.println("Manager  : " + Courier_Manager);
 		System.out.println("Salary   : " + Courier_Salary);
+		System.out.println("");
+	}
+}
+
+// class customer used for customers
+class Customer extends Person {
+	static int id = 1;
+	int Customer_ID;
+	String Customer_Address;
+
+	// default constructor is used to assign default data to a new object. Will not be used so can be removed
+	Customer() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		this.Customer_ID = id++;
+		super.add();
+
+		System.out.println("Enter Address:");
+		this.Customer_Address = br.readLine();
+	}
+	// protected function only usable in this class and subclasses
+	protected void display() {
+		System.out.println("ID       : " + Customer_ID);
+		super.display();
+		System.out.println("Address  : " + Customer_Address);
+		System.out.println("");
+	}
+}
+
+// class Shipment used for Shipments
+class Shipment {
+	static int id = 1;
+	int Shipment_ID;
+	Customer Shipment_Sender;
+	Customer Shipment_Reciever;
+	float Shipment_Price;	
+	String Shipment_Hub;
+	String Shipment_Courier;
+	String Shipment_Status;
+
+	Shipment() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		this.Shipment_ID = id++;
+
+		System.out.println("Enter Sender Details:");
+		Shipment_Sender = new Customer();
+		
+		System.out.println("Enter Reciever Details:");
+		Shipment_Reciever = new Customer();
+
+		System.out.println("Enter price of the shipment:");
+		this.Shipment_Price = Float.parseFloat(br.readLine());
+
+		System.out.println("Enter current hub location:");
+		this.Shipment_Hub = br.readLine();
+
+		System.out.println("Enter current courier incharge:");
+		this.Shipment_Courier = br.readLine();
+
+		this.Shipment_Status = "Yet to send";
+	}
+
+	void update_status() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Update shipment status\nCurrent Status = " + Shipment_Status);
+		System.out.println("\n1. 'In Transit'\n2. 'In Hub'\n3. 'Out for Delivery'\n4. 'Delivered'\n5. 'Not Delivered'\n6. 'Returned to Sender'\n7. No Change\n\nSelect new status:");
+		int choice = Integer.parseInt(br.readLine());
+		switch (choice) {
+			case 1:
+				this.Shipment_Status = "In Transit";
+				break;
+			case 2:
+				this.Shipment_Status = "In Hub";
+				break;
+			case 3:
+				this.Shipment_Status = "Out for Delivery";
+				break;
+			case 4:
+				this.Shipment_Status = "Delivered";
+				break;
+			case 5:
+				this.Shipment_Status = "Not Delivered";
+				break;
+			case 6:
+				this.Shipment_Status = "Returned to Sender";
+				break;
+			default:
+				break;
+		}
+		System.out.println("\nCurrent Status = " + Shipment_Status);
+	}
+
+	void update_details() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Update Hub and Courier details");
+		System.out.println("\nCurrent Hub = " + Shipment_Hub + "\nCurrent Courier = " + Shipment_Courier);
+		System.out.println("\n1. Update Hub and Courier\n2. Update Hub\n3. Update Courier\n4. No Change\n\nSelect module:");
+		int choice = Integer.parseInt(br.readLine());
+		switch (choice) {
+			case 1:
+				System.out.println("Enter current hub location:");
+				this.Shipment_Hub = br.readLine();
+
+				System.out.println("Enter current courier incharge:");
+				this.Shipment_Courier = br.readLine();
+
+				break;
+			case 2:
+				System.out.println("Enter current hub location:");
+				this.Shipment_Hub = br.readLine();
+				break;
+			case 3:
+				System.out.println("Enter current courier incharge:");
+				this.Shipment_Courier = br.readLine();
+				break;
+			default:
+				break;
+		}
+		System.out.println("\nCurrent Status = " + Shipment_Status);
+	}
+
+	void display() throws IOException {
+		System.out.println("***********Shipment " + Shipment_ID + "***********");
+
+		System.out.println("Sender:\n");
+		Shipment_Sender.display();
+		System.out.println("--------------------------------");
+
+		Shipment_Reciever.display();
+		System.out.println("--------------------------------");
+		
+		System.out.println("Price    : " + Shipment_Price);
+		System.out.println("Hub      : " + Shipment_Hub);
+		System.out.println("Courier  : " + Shipment_Courier);
+		System.out.println("Status   : " + Shipment_Status);
 		System.out.println("");
 	}
 }
